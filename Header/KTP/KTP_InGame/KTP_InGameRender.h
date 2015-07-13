163ef473e4acd53587497848dc98f5a940d193cb
장은준 <jangej1031@naver.com>
2015-07-13 11:22:25 +0900

    Create KTP_InGameRender.h

---
+++ Header/KTP/KTP_InGame/KTP_InGameRender.h
@@ -0,0 +1,20 @@
+#ifndef __KTP_INGAMERENDER_H_
+#define __KTP_INGAMERENDER_H_
+
+void KTP_InGameRender(bool Radar);
+
+void KTP_InGameRenderInit();
+
+void KTP_InGameRenderRelease();
+
+void KTP_InGameRenderGameOver();
+
+void KTP_InGameRenderPhone();
+
+void KTP_InGameRenderTeacher();
+
+void KTP_InGameRenderRadar();
+
+void KTP_InGameRenderDanger();
+
+#endif
