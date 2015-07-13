97bc4799b1dde648f6bad4465a8910a2d986bf29
장은준 <jangej1031@naver.com>
2015-07-13 11:17:27 +0900

    Create common.h

---
+++ Header/common.h
@@ -0,0 +1,20 @@
+#ifndef __COMMON_H_
+#define __COMMON_H_
+
+
+//표준 헤더파일
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+
+//SDL라이브러리 헤더파일
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_Main.h>
+
+//FMOD라이브러리 헤더파일
+#include <FMOD.h>
+
+#endif
